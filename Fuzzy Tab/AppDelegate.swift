6e7e4aa6b9ff97a72984b6ca28bd01d4943cc66e
rv <rvdeguzman@protonmail.com>
2026-03-28 14:08:52 -0400

    Initial Commit

---
+++ Fuzzy Tab/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Fuzzy Tab
+//
+//  Created by rv on 2026-03-28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ Fuzzy TabTests/Fuzzy_TabTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fuzzy_TabTests.swift
+//  Fuzzy TabTests
+//
+//  Created by rv on 2026-03-28.
+//
+
+import Testing
+@testable import Fuzzy_Tab
+
+struct Fuzzy_TabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
